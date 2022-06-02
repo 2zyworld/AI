3e7e89130caed58ea0cf154a6f30b9f6f4e6c639
Seunghwan Lee <reconfiguration@protonmail.com>
2022-06-02 09:24:37 +0900

    Add new dataset class for inference
    Added new dataset class `BERTDatasetInference` for the inference, which is identical to the existing class `BERTDataset` but without labeling process.

--- kobert/data.py
+++ kobert/data.py
@@ -16,3 +16,17 @@ class BERTDataset(Dataset):
 
     def __len__(self):
         return len(self.labels)
+
+
+class BERTDatasetInference(Dataset):
+    def __init__(self, dataset, bert_tokenizer, max_len, pad, pair):
+        transform = nlp.data.BERTSentenceTransform(
+            bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair
+        )
+        self.sentences = [transform([data]) for data in dataset]
+
+    def __getitem__(self, item):
+        return self.sentences[item]
+
+    def __len__(self):
+        return len(self.sentences)
