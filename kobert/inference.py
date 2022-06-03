21d85639728a6fb167aa7bae4598a6749b3f67cb
Seunghwan Lee <reconfiguration@protonmail.com>
2022-06-03 09:49:29 +0900

    Add `emotion` function
    The function `emotion` takes output of 60-class list and returns 8-class list of emotions.

--- kobert/inference.py
+++ kobert/inference.py
@@ -8,10 +8,10 @@ from AI.kobert.models import BERTClassifier
 from AI.kobert.data import BERTDatasetInference
 
 
-def inference(sentence):
+def inference(sentence, model_path="model1.pt"):
     bert_model, vocab = get_pytorch_kobert_model()
     model = BERTClassifier(bert_model, dr_rate=0.5)
-    model.load_state_dict(torch.load("model.pt"))
+    model.load_state_dict(torch.load(model_path))
     model.eval()
 
     tokenizer = get_tokenizer()
@@ -26,7 +26,8 @@ def inference(sentence):
         token_ids = token_ids.long().to(device)
         segment_ids = segment_ids.long().to(device)
         output.append(model(token_ids, valid_length, segment_ids))
-
+    softmax = torch.nn.Softmax(dim=1)
+    output = softmax(output[0])[0].tolist()
     return output
 
 
@@ -37,6 +38,23 @@ def opacity(color_code, amp, bg=False):
         c = (1 - amp) * bg + amp * color_code[i]
         modified.append(int(c))
     return modified
+
+
+def emotion(output, encoder):
+    temp1 = []
+    for idx in range(60):
+        if idx + 10 == 51:
+            code = 31
+        elif idx + 10 == 59:
+            code = 34
+        else:
+            code = idx + 10
+
+        row = encoder[encoder.code == code].iloc[0, 2:10]
+        temp2 = [output[idx] * x for x in row]
+        temp1.append(temp2)
+    normalize = np.sum(np.array(temp1), axis=0)
+    return normalize
 
 
 def colorize(output, color_encoder):
